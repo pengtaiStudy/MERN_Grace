81405249382c309397b300a668683be067430e1e
suekim59 <74885313+suekim59@users.noreply.github.com>
2021-11-20 07:16:44 -0800

    feat:add login&register pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Settings />
+      <Register />
     </>
   );
 }
