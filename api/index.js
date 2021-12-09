36a7a056699de4195e9e826d2a0b905ae4f6a4f4
suekim59 <74885313+suekim59@users.noreply.github.com>
2021-12-09 07:36:24 -0800

    feat:add post CRUD

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 app.use(express.json());
@@ -29,6 +30,7 @@ app.use("/", (req, res) => {
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen("3000", () => {
     console.log("Backend is running");
