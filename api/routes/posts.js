66d027fef037635fd9b72fe2ce3bc672d1daa0d5
suekim59 <74885313+suekim59@users.noreply.github.com>
2021-12-09 08:00:56 -0800

    fix:typo

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
-const bcrypt = require("bcrypt");
 
 //Create
 router.post('/', async (req, res)=> {
