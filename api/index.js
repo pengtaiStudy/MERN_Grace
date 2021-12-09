a5e65bcbea03f78cd011c7792799fc5844777d14
suekim59 <74885313+suekim59@users.noreply.github.com>
2021-12-09 08:00:33 -0800

    feat:add upload files REST  API

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -22,15 +24,24 @@ mongoose
     .connect(process.env.MONGO_URL)
     .then(console.log("Connected to MongoDB."))
     .catch((err)=> console.log(err));
-/*
-app.use("/", (req, res) => {
-    console.log("test");
-});
-*/
+
+    const storage = multer.diskStorage({
+        destination:(req,file,cb) =>{
+            cb(null,"images");
+        },filename : (req,file,cb)=>{
+            cb(null, req.body.filename);
+        },
+    })
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"), (req, res)=>{
+    res.status(200).json("File has been uploaded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("3000", () => {
     console.log("Backend is running");
