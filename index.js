3cb1cf37806d55743318a10d1b67fe6bcfeb3533
suekim59 <74885313+suekim59@users.noreply.github.com>
2022-01-23 08:19:18 -0800

    fix:static index.js

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-  });
-
 mongoose
     .connect(process.env.MONGO_URL)
     .then(console.log("Connected to MongoDB."))
@@ -41,6 +36,12 @@ app.use("/auth", authRoute);
 app.use("/users", userRoute);
 app.use("/posts", postRoute);
 app.use("/categories", categoryRoute);
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running");
